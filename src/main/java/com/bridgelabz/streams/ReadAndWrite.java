a88afb1ba740de9cdf5ec2802896bb8f5ea4d5ec
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-03-31 22:29:50 +0530

    [Nitesh] ADD:inital commit

---
+++ src/main/java/com/bridgelabz/streams/ReadAndWrite.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.streams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ReadAndWrite {
+    public static void main(String[] args){
+        try(FileInputStream fi=new FileInputStream("input.txt");
+        FileOutputStream fo=new FileOutputStream("output")){
+            int data;
+            while((data=fi.read())!=-1){
+                fo.write(data);
+            }
+            System.out.println("data copied");
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
